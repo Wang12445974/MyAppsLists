aa1dcc3f5c382f41e9c5b139bc1c178dec9d093b
Wang chong <12445975@qq.com>
2017-08-15 17:12:23 +0800

    轮播图

---
+++ app/src/main/java/com/example/hoperun/myappslists/LunBoActivity.java
@@ -0,0 +1,90 @@
+package com.example.hoperun.myappslists;
+
+import android.graphics.Picture;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.BaseViewPagerAdapter;
+import view.AutoScrollViewPager;
+
+/**
+ * Created by Hoperun on 2017/8/15.
+ */
+
+public class LunBoActivity extends AppCompatActivity {
+    private BaseViewPagerAdapter<String> mBaseViewPagerAdapter;
+
+    private AutoScrollViewPager mViewPager;
+
+    private String[] paths = {"https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=c493b482b47eca800d053ee7a1229712/8cb1cb1349540923abd671df9658d109b2de49d7.jpg",
+            "https://ss1.baidu.com/9vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=ff0999f6d4160924c325a51be406359b/86d6277f9e2f070861ccd4a0ed24b899a801f241.jpg"};
+
+//    private String[] paths = {"https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=c493b482b47eca800d053ee7a1229712/8cb1cb1349540923abd671df9658d109b2de49d7.jpg",
+//            "https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=45fbfa5555da81cb51e684cd6267d0a4/2f738bd4b31c8701491ea047237f9e2f0608ffe3.jpg",
+//            "https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=ae0e95c0fc1986185e47e8847aec2e69/0b46f21fbe096b63eb314ef108338744ebf8ac62.jpg",
+//            "https://ss3.baidu.com/9fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=1fad2b46952397ddc9799f046983b216/dc54564e9258d109c94bbb13d558ccbf6d814de2.jpg",
+//            "https://ss1.baidu.com/9vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=ff0999f6d4160924c325a51be406359b/86d6277f9e2f070861ccd4a0ed24b899a801f241.jpg"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lunbo);
+
+        mViewPager = (AutoScrollViewPager) findViewById(R.id.viewPager);
+
+        mBaseViewPagerAdapter = new BaseViewPagerAdapter<String>(this,listener) {
+            @Override
+            public void loadImage(ImageView view, int position, String url) {
+                Picasso.with(LunBoActivity.this).load(url).into(view);
+            }
+
+            @Override
+            public void setSubTitle(TextView textView, int position, String s) {
+                textView.setText(s);
+            }
+        };
+        mViewPager.setAdapter(mBaseViewPagerAdapter);
+
+        mBaseViewPagerAdapter.add(initData());
+
+
+    }
+
+    private List<String> initData() {
+        List<String> data = new ArrayList<>();
+        Picture picture ;
+        for (int i = 0 ; i < paths.length ;i++){
+            data.add(paths[i]);
+        }
+        return data;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mViewPager.onDestroy();
+    }
+
+    public void add(View view) {
+        mBaseViewPagerAdapter.add("http://img3.imgtn.bdimg.com/it/u=214931719,1608091472&fm=21&gp=0.jpg");
+    }
+
+    private BaseViewPagerAdapter.OnAutoViewPagerItemClickListener listener = new BaseViewPagerAdapter.OnAutoViewPagerItemClickListener<String>() {
+
+        @Override
+        public void onItemClick(int position, String url) {
+            Toast.makeText(getApplicationContext(),
+                    position + " ========= "+ url, Toast.LENGTH_SHORT).show();
+        }
+    };
+}
---
+++ app/src/main/java/com/example/hoperun/myappslists/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.hoperun.myappslists;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void lunbo(View view){
+        Intent intent=new Intent(MainActivity.this,LunBoActivity.class);
+        startActivity(intent);
+    }
+}
